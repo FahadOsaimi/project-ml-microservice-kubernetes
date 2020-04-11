d62cf9b9572887b93a3c831e0291ce64a635e2f5
Fahad <fahad.osaimi@gmail.com>
2020-04-11 21:36:51 +0300

    task 7

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,11 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build --tag=fahadosaimi/demolocal .
 # Step 2: 
 # List docker images
-
+docker image ls 
 # Step 3: 
 # Run flask app
+# docker run -p 8080:8080 IMAGE_ID
+docker run -p 8000:80  -it fahadosaimi/demolocal bash
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,29 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="fahadosaimi/demolocal"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run notebook\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=notebook
+
+
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
 
+echo "sleeping for 180 seconds while waiting for pod to come up"
+
+sleep 180
+
+kubectl port-forward deployment/demolocal 8000:80 
+
+kubectl logs --selector app=demolocal
